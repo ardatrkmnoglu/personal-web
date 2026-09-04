82ec7ed5b1499d754eb6640a005561c8ad4185fe
Arda Türkmenoğlu <ardatrkmnoglu@outlook.com>
2026-09-04 04:32:23 +0300

    fix: prevent new calculations from appending to error messages or invalid values [apps/calc] minor: improved error handling for invalid calculations [apps/calc] chore: reformat calc.html and calc.js

--- apps/calc.js
+++ apps/calc.js
@@ -1,18 +1,30 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input) {
-    display.value += input;
+  if (display.value == "Error" ||
+    display.value == "NaN" ||
+    display.value == "Infinity" ||
+    display.value == "undefined") {
+    clearDisplay();
+  }
+  display.value += input;
 }
 
 function calculate() {
-    try {
-        display.value = eval(display.value);
-    }
-    catch (error) {
-        display.value = "Error";
-    }
+  if (display.value == "Error" ||
+    display.value == "NaN" ||
+    display.value == "Infinity" ||
+    display.value == "undefined") {
+    clearDisplay();
+  }
+  try {
+    display.value = eval(display.value);
+  }
+  catch (error) {
+    display.value = "Error";
+  }
 }
 
 function clearDisplay() {
-    display.value = "";
+  display.value = "";
 }
